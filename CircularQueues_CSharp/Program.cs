ca80afd17444673d89e9facd3ebc85d4b3c49d20
Siti Marfungah Dwi Lestari <siti.marfungah.ft21@mail.umy.ac.id>
2022-12-16 17:12:50 +0700

    declare variables

--- CircularQueues_CSharp/Program.cs
+++ CircularQueues_CSharp/Program.cs
@@ -2,8 +2,11 @@
 
 namespace CircularQueues_CSharp
 {
-    class Program
+    class Queues
     {
+
+        int FRONT, REAR, max = 5;
+        int[] queue_array = new int[5];
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
