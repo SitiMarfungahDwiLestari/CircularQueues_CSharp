04e18526fb7cc556a5696b8d9bc82840f9c60b36
Siti Marfungah Dwi Lestari <siti.marfungah.ft21@mail.umy.ac.id>
2022-12-16 17:21:39 +0700

    If REAR is not at the last position, then its value is incremented by one.

--- CircularQueues_CSharp/Program.cs
+++ CircularQueues_CSharp/Program.cs
@@ -32,7 +32,9 @@ namespace CircularQueues_CSharp
                 //If REAR is at the last position of the array, then the value of REAR is set to 0 that corresponds to the first position of the array
                 if (REAR == max - 1)
                     REAR = 0;
-
+                else
+                    //If REAR is not at the last position, then its value is incremented by one.
+                    REAR = REAR + 1;
             }
         }
         static void Main(string[] args)
