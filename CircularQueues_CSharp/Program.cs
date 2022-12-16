84270dd63f3545337b3555539d965e4d4b73af38
Siti Marfungah Dwi Lestari <siti.marfungah.ft21@mail.umy.ac.id>
2022-12-16 17:22:35 +0700

    Once the position of REAR is determined, the element is added at its proper place

--- CircularQueues_CSharp/Program.cs
+++ CircularQueues_CSharp/Program.cs
@@ -36,6 +36,8 @@ namespace CircularQueues_CSharp
                     //If REAR is not at the last position, then its value is incremented by one.
                     REAR = REAR + 1;
             }
+            //Once the position of REAR is determined, the element is added at its proper place
+            queue_array[REAR] = element;
         }
         static void Main(string[] args)
         {
