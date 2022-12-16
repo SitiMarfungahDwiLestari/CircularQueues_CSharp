ffaabf57a5cc60bf2748cd522236e731f37e847b
Siti Marfungah Dwi Lestari <siti.marfungah.ft21@mail.umy.ac.id>
2022-12-16 17:13:07 +0700

    Initializing the values of the variables REAR and FRONT to -1 to indicate that the queues is initially empty

--- CircularQueues_CSharp/Program.cs
+++ CircularQueues_CSharp/Program.cs
@@ -7,6 +7,12 @@ namespace CircularQueues_CSharp
 
         int FRONT, REAR, max = 5;
         int[] queue_array = new int[5];
+        public Queues()
+        {
+            //Initializing the values of the variables REAR and FRONT to -1 to indicate that the queues is initially empty
+            FRONT = -1;
+            REAR = -1;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
